e33642f5223efd24e8a7f88b7c36514dda3d5512
Marvin Dawson <marvintdawson@gmail.com>
2017-10-03 21:26:21 -0500

    added onClose function for inspection jobs

--- src/pages/jobs-profile/atco-industries/atco-industries.ts
+++ src/pages/jobs-profile/atco-industries/atco-industries.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
 import {JobRevisionsPage} from "../../job-revisions/job-revisions";
 
 @Component({
@@ -14,7 +14,16 @@ export class AtcoIndustriesPage{
   revStatus: string = "NEW";
   navToJobRevisionPage = JobRevisionsPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public altCtrl: AlertController) {
+  }
+
+  if(closeJob){
+    this.altCtrl.create({
+      title:  'Close' + this.jobTitle,
+      message: 'You are about to close this job to the entire team.  Are you sure you want to proceed?',
+    });
   }
 
 }
